49e8f5176428aa109aa59dfbb442be5bde33a2b9
Slaurmagan <s.hrakovich33@mail.ru>
2020-11-28 07:38:07 +0300

    Added sides controls

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 import Carousel from "./components/Carousel/Carousel"
 
 
-render(<Carousel infinite={false} min_distance={50} />,document.getElementById('root'));
+render(<Carousel infinite={false} min_distance={50} side_control_distance={100}/>,document.getElementById('root'));
